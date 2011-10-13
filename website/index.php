66d0dce5ea71b7e0bc5c74de74137666f9db0496
McLeopold <mcleopold@gmail.com>
2011-10-12 21:55:51 -0700

    update to index.php

--- website/index.php
+++ website/index.php
@@ -1,22 +1,18 @@
 <?php
-include('header.php');
-require_once('memcache.php');
 
-if(file_exists('server_message.html')) {
-    //Used to convey a message on the front page
-    include('server_message.html');
-}
+require_once('header.php');
+require_once('memcache.php');
 
 ?>
 
-<!--<MarkdownReplacement with="competition.md">--><h1>Introducing Ants</h1>
+<?php if (logged_in_with_valid_credentials()) { ?>
+<!--<MarkdownReplacement with="competition-Announcement.md">--><!--</MarkdownReplacement>-->
+<?php } else { ?>
+<!--<MarkdownReplacement with="competition-Splash.md">--><!--</MarkdownReplacement>-->
+<?php } ?>
 
-<p>The Google AI Challenge is all about creating artificial intelligence, whether you are a beginning programmer or an expert. Using one of the easy-to-use starter kits, you will create a computer program (in any language) that controls a colony of ants which fight against other colonies for domination.</p>
+<!--<MarkdownReplacement with="competition.md">--><!--</MarkdownReplacement>-->
 
-<p>It only takes 5 minutes to submit one of the starter kits to the website and watch your ant colony fight for domination against colonies created by other people from around the world. From there check out the tutorials on how to locally run your bot and begin programming!</p>
-<!--</MarkdownReplacement>-->
-
-<p>Computer Programs Duking it Out with Ants:</p>
 <?php
     $last_game_id = 0;
     if ($memcache)
@@ -30,4 +26,8 @@ if(file_exists('server_message.html')) {
 
 <p>Browse other <a href="games.php">recent games here</a>.</p>
 
-<?php include 'footer.php'; ?>
+<?php
+
+require_once('footer.php');
+
+?>
